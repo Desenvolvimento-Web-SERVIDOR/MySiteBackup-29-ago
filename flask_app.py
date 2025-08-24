bf1dab6825fe13832496d989b6fecd87c5016a4f
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-08-24 20:02:47 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
