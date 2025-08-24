922a2be0e1c41e3cf887503682a88fa28a5ad0cc
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-08-24 19:52:46 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,44 +1,24 @@
 from flask import Flask, request
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 
 app = Flask(__name__)
+bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return '''
-    <h1><strong>Avaliação contínua: Aula 030</strong></h1>
-    <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/user/Rogerio%20Santos%20Barbosa/PT3032051/IFSP">Identificação</a></li>
-        <li><a href="/contextorequisicao">Contexto da Requisição</a></li>
-    </ul>
+    return render_template('index.html', current_time=datetime.utcnow())
 
-    '''
+@app.route('/user/<nome>')
+def user(nome):
 
-@app.route('/user/<nome>/<prontuario>/<instituicao>')
-def identificacao(nome, prontuario, instituicao):
+    return render_template('user.html')
 
-    return f'''
-        <h1><strong>Avaliação contínua: Aula 030</strong></h1>
-        <h2><strong>Aluno: {nome}</strong></h2>
-        <h2><strong>Prontuário: {prontuario}</strong></h2>
-        <h2><strong>Instituição: {instituicao}</strong></h2>
-        <br>
-        <a href="/">Voltar à página inicial</a>
-    '''
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
-@app.route('/contextorequisicao')
-def contextorequisicao():
-    # 1. Obtendo as informações do objeto 'request'
-    user_agent = request.headers.get('User-Agent') # Pega a string do navegador (ex: Chrome, Firefox)
-    user_ip = request.remote_addr 
-    host = request.host # Pega o domínio onde a aplicação está rodando
-    
-    # 2. Injetando as informações no HTML com uma f-string
-    return f'''
-        <h1><strong>Avaliação contínua: Aula 030</strong></h1>
-        <h2><strong>Seu navegador é:</strong> {user_agent}</h2>
-        <h2><strong>O IP do computador remoto é:</strong> {user_ip}</h2>
-        <h2><strong>O host da aplicação é:</strong> {host}</h2>
-        <br>
-        <a href="/">Voltar à página inicial</a>
-    '''
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
