72ef1cd3c66da501151a1c1496fc8a242727edb6
RogerioSantos <barbosa.rogerio@aluno.ifsp.edu.br>
2025-08-24 23:55:21 +0000

    Corrigindo essa desgraça

--- flask_app.py
+++ flask_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
