41ea3d69078412e7281613f9bdedf63636fc6c7e
Rogério Santos Barbosa <DevSemCash@gmail.com>
2025-09-07 18:34:49 -0300

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, render_template
+from datetime import datetime
+from flask import Flask, render_template, request, make_response, redirect, abort
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from datetime import datetime
 
 app = Flask(__name__)
+
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
@@ -11,9 +12,23 @@ moment = Moment(app)
 def index():
     return render_template('index.html', current_time=datetime.utcnow())
 
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', user = name)
+@app.route('/user/<name>/<prontuario>/<instituicao')
+def user(name, prontuario, instituicao):
+    return render_template('user.html', 
+                           name=name,
+                           prontuario=prontuario,
+                           instituicao=instituicao);
+
+@app.route('/contextorequisicao/<name>')
+def contextorequisicao(name):
+    user_agent = request.headers.get('User-Agent');
+    remote_addr = request.remote_addr;
+    remote_host = request.host;
+    return render_template('contexto.html',
+                           name=name,
+                           user_agent=user_agent,
+                           remote_addr=remote_addr,
+                           remote_host=remote_host);
 
 @app.errorhandler(404)
 def page_not_found(e):
